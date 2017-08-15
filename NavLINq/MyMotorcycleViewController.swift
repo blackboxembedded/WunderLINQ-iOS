e2e8f32ac206ff3e07560727f36471b5a65a1111
Keith Conger <keith.conger@gmail.com>
2017-08-14 19:32:27 -0600

    Initial commit.  Includes BLE message reception and Compass Page

---
+++ NavLINq/MyMotorcycleViewController.swift
@@ -0,0 +1,363 @@
+//
+//  ViewController.swift
+//  NavLINq
+//
+//  Created by Keith Conger on 8/13/17.
+//  Copyright © 2017 Keith Conger. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class MyMotorcycleViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var disconnectButton: UIBarButtonItem!
+    
+    var centralManager:CBCentralManager!
+    var navLINq:CBPeripheral?
+    var messageCharacteristic:CBCharacteristic?
+    
+    let deviceName = "NavLINq"
+    
+    // define our scanning interval times
+    let timerPauseInterval:TimeInterval = 10.0
+    let timerScanInterval:TimeInterval = 2.0
+    
+    var keepScanning = false
+    
+    var lastMessage = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        centralManager = CBCentralManager(delegate: self,
+                                          queue: nil)
+        
+        messageLabel.text = "Searching"
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Handling User Interaction
+    
+    @IBAction func handleDisconnectButtonTapped(_ sender: AnyObject) {
+        // if we don't have a sensor tag, start scanning for one...
+        if navLINq == nil {
+            keepScanning = true
+            resumeScan()
+            return
+        } else {
+            disconnect()
+        }
+    }
+    
+    func disconnect() {
+        if let navLINq = self.navLINq {
+            if let mc = self.messageCharacteristic {
+                navLINq.setNotifyValue(false, for: mc)
+            }
+
+            
+            /*
+             NOTE: The cancelPeripheralConnection: method is nonblocking, and any CBPeripheral class commands
+             that are still pending to the peripheral you’re trying to disconnect may or may not finish executing.
+             Because other apps may still have a connection to the peripheral, canceling a local connection
+             does not guarantee that the underlying physical link is immediately disconnected.
+             
+             From your app’s perspective, however, the peripheral is considered disconnected, and the central manager
+             object calls the centralManager:didDisconnectPeripheral:error: method of its delegate object.
+             */
+            centralManager.cancelPeripheralConnection(navLINq)
+        }
+        messageCharacteristic = nil
+    }
+
+    
+    // MARK: - Bluetooth scanning
+    
+    func pauseScan() {
+        // Scanning uses up battery on phone, so pause the scan process for the designated interval.
+        print("*** PAUSING SCAN...")
+        _ = Timer(timeInterval: timerPauseInterval, target: self, selector: #selector(resumeScan), userInfo: nil, repeats: false)
+        centralManager.stopScan()
+        disconnectButton.isEnabled = true
+    }
+    
+    func resumeScan() {
+        if keepScanning {
+            // Start scanning again...
+            print("*** RESUMING SCAN!")
+            disconnectButton.isEnabled = false
+            messageLabel.text = "Searching"
+            _ = Timer(timeInterval: timerScanInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            disconnectButton.isEnabled = true
+        }
+    }
+    
+    // MARK: - Updating UI
+    
+    func updateMessageDisplay() {
+
+        messageLabel.text = lastMessage
+    }
+    
+    func displayMessage(_ data:Data) {
+        // We'll get four bytes of data back, so we divide the byte count by two
+        // because we're creating an array that holds two 16-bit (two-byte) values
+        let dataLength = data.count / MemoryLayout<UInt8>.size
+        var dataArray = [UInt8](repeating: 0, count: dataLength)
+        (data as NSData).getBytes(&dataArray, length: dataLength * MemoryLayout<Int16>.size)
+        
+        // output values for debugging/diagnostic purposes
+        var messageString = ""
+        for i in 0 ..< dataLength {
+            let nextInt:UInt8 = dataArray[i]
+            //print("next int: \(nextInt)")
+            messageString += "\(nextInt),"
+        }
+        print(messageString)
+        lastMessage = messageString
+        if UIApplication.shared.applicationState == .active {
+            updateMessageDisplay()
+        }
+    }
+
+    
+    // MARK: - CBCentralManagerDelegate methods
+    
+    // Invoked when the central manager’s state is updated.
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        var showAlert = true
+        var message = ""
+        
+        switch central.state {
+        case .poweredOff:
+            message = "Bluetooth on this device is currently powered off."
+        case .unsupported:
+            message = "This device does not support Bluetooth Low Energy."
+        case .unauthorized:
+            message = "This app is not authorized to use Bluetooth Low Energy."
+        case .resetting:
+            message = "The BLE Manager is resetting; a state update is pending."
+        case .unknown:
+            message = "The state of the BLE Manager is unknown."
+        case .poweredOn:
+            showAlert = false
+            message = "Bluetooth LE is turned on and ready for communication."
+            
+            print(message)
+            keepScanning = true
+            _ = Timer(timeInterval: timerScanInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+            
+            // Initiate Scan for Peripherals
+            //Option 1: Scan for all devices
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+            
+            // Option 2: Scan for devices that have the service you're interested in...
+            //let sensorTagAdvertisingUUID = CBUUID(string: Device.SensorTagAdvertisingUUID)
+            //print("Scanning for SensorTag adverstising with UUID: \(sensorTagAdvertisingUUID)")
+            //centralManager.scanForPeripheralsWithServices([sensorTagAdvertisingUUID], options: nil)
+            
+        }
+        
+        if showAlert {
+            let alertController = UIAlertController(title: "Central Manager State", message: message, preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            alertController.addAction(okAction)
+            self.show(alertController, sender: self)
+        }
+    }
+
+    /*
+     Invoked when the central manager discovers a peripheral while scanning.
+     
+     The advertisement data can be accessed through the keys listed in Advertisement Data Retrieval Keys.
+     You must retain a local copy of the peripheral if any command is to be performed on it.
+     In use cases where it makes sense for your app to automatically connect to a peripheral that is
+     located within a certain range, you can use RSSI data to determine the proximity of a discovered
+     peripheral device.
+     
+     central - The central manager providing the update.
+     peripheral - The discovered peripheral.
+     advertisementData - A dictionary containing any advertisement data.
+     RSSI - The current received signal strength indicator (RSSI) of the peripheral, in decibels.
+     
+     */
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        //print("centralManager didDiscoverPeripheral - CBAdvertisementDataLocalNameKey is \"\(CBAdvertisementDataLocalNameKey)\"")
+        
+        // Retrieve the peripheral name from the advertisement data using the "kCBAdvDataLocalName" key
+        if let peripheralName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            print("NEXT PERIPHERAL NAME: \(peripheralName)")
+            print("NEXT PERIPHERAL UUID: \(peripheral.identifier.uuidString)")
+            
+            if peripheralName == deviceName {
+                print("SENSOR TAG FOUND! ADDING NOW!!!")
+                // to save power, stop scanning for other devices
+                keepScanning = false
+                disconnectButton.isEnabled = true
+                
+                // save a reference to the sensor tag
+                navLINq = peripheral
+                navLINq!.delegate = self
+                
+                // Request a connection to the peripheral
+                centralManager.connect(navLINq!, options: nil)
+            }
+        }
+    }
+
+    /*
+     Invoked when a connection is successfully created with a peripheral.
+     
+     This method is invoked when a call to connectPeripheral:options: is successful.
+     You typically implement this method to set the peripheral’s delegate and to discover its services.
+     */
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("**** SUCCESSFULLY CONNECTED NavLINq!!!")
+        
+        messageLabel.text = "Connected"
+        
+        // Now that we've successfully connected to the SensorTag, let's discover the services.
+        // - NOTE:  we pass nil here to request ALL services be discovered.
+        //          If there was a subset of services we were interested in, we could pass the UUIDs here.
+        //          Doing so saves battery life and saves time.
+        peripheral.discoverServices(nil)
+    }
+    
+    
+    /*
+     Invoked when the central manager fails to create a connection with a peripheral.
+     
+     This method is invoked when a connection initiated via the connectPeripheral:options: method fails to complete.
+     Because connection attempts do not time out, a failed connection usually indicates a transient issue,
+     in which case you may attempt to connect to the peripheral again.
+     */
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("**** CONNECTION NavLINq FAILED!!!")
+    }
+    
+    
+    /*
+     Invoked when an existing connection with a peripheral is torn down.
+     
+     This method is invoked when a peripheral connected via the connectPeripheral:options: method is disconnected.
+     If the disconnection was not initiated by cancelPeripheralConnection:, the cause is detailed in error.
+     After this method is called, no more methods are invoked on the peripheral device’s CBPeripheralDelegate object.
+     
+     Note that when a peripheral is disconnected, all of its services, characteristics, and characteristic descriptors are invalidated.
+     */
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        print("**** DISCONNECTED NavLINq!!!")
+        messageLabel.text = "Tap to search"
+        if error != nil {
+            print("****** DISCONNECTION DETAILS: \(error!.localizedDescription)")
+        }
+        navLINq = nil
+    }
+    
+    
+    //MARK: - CBPeripheralDelegate methods
+    
+    /*
+     Invoked when you discover the peripheral’s available services.
+     
+     This method is invoked when your app calls the discoverServices: method.
+     If the services of the peripheral are successfully discovered, you can access them
+     through the peripheral’s services property.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    // When the specified services are discovered, the peripheral calls the peripheral:didDiscoverServices: method of its delegate object.
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if error != nil {
+            print("ERROR DISCOVERING SERVICES: \(error?.localizedDescription)")
+            return
+        }
+        
+        // Core Bluetooth creates an array of CBService objects —- one for each service that is discovered on the peripheral.
+        if let services = peripheral.services {
+            for service in services {
+                print("Discovered service \(service)")
+                // If we found either the temperature or the humidity service, discover the characteristics for those services.
+                if (service.uuid == CBUUID(string: Device.NavLINqServiceUUID)) {
+                    peripheral.discoverCharacteristics(nil, for: service)
+                }
+            }
+        }
+    }
+    
+    /*
+     Invoked when you discover the characteristics of a specified service.
+     
+     If the characteristics of the specified service are successfully discovered, you can access
+     them through the service's characteristics property.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if error != nil {
+            print("ERROR DISCOVERING CHARACTERISTICS: \(error?.localizedDescription)")
+            return
+        }
+        
+        if let characteristics = service.characteristics {
+            
+            for characteristic in characteristics {
+                // Message Data Characteristic
+                if characteristic.uuid == CBUUID(string: Device.MessageCharacteristicUUID) {
+                    // Enable the message notifications
+                    messageCharacteristic = characteristic
+                    navLINq?.setNotifyValue(true, for: characteristic)
+                }
+                
+                
+            }
+        }
+    }
+    
+    
+    /*
+     Invoked when you retrieve a specified characteristic’s value,
+     or when the peripheral device notifies your app that the characteristic’s value has changed.
+     
+     This method is invoked when your app calls the readValueForCharacteristic: method,
+     or when the peripheral notifies your app that the value of the characteristic for
+     which notifications and indications are enabled has changed.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if error != nil {
+            print("ERROR ON UPDATING VALUE FOR CHARACTERISTIC: \(characteristic) - \(error?.localizedDescription)")
+            return
+        }
+        
+        // extract the data from the characteristic's value property and display the value based on the characteristic type
+        if let dataBytes = characteristic.value {
+            if characteristic.uuid == CBUUID(string: Device.MessageCharacteristicUUID) {
+                displayMessage(dataBytes)
+        }
+    }
+    
+    
+    // MARK: - Utility Methods
+    
+    func convertCelciusToFahrenheit(_ celcius:Double) -> Double {
+        let fahrenheit = (celcius * 1.8) + Double(32)
+        return fahrenheit
+    }
+
+    }
+
+}
+
---
+++ NavLINq/UserDefauts+Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  UserDefauts+Extensions.swift
+//  NavLINq
+//
+//  Created by Keith Conger on 8/13/17.
+//  Copyright © 2017 Keith Conger. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+extension UserDefaults {
+    var currentLocation: CLLocation {
+        get { return CLLocation(latitude: latitude ?? 90, longitude: longitude ?? 0) } // default value is North Pole (lat: 90, long: 0)
+        set { latitude = newValue.coordinate.latitude
+            longitude = newValue.coordinate.longitude }
+    }
+    
+    private var latitude: Double? {
+        get {
+            if let _ = object(forKey: #function) {
+                return double(forKey: #function)
+            }
+            return nil
+        }
+        set { set(newValue, forKey: #function) }
+    }
+    
+    private var longitude: Double? {
+        get {
+            if let _ = object(forKey: #function) {
+                return double(forKey: #function)
+            }
+            return nil
+        }
+        set { set(newValue, forKey: #function) }
+    }
+}
