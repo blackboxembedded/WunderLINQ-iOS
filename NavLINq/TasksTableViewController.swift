29bddf7248e1d8020d851fff534c6340c0b93d1a
Keith Conger <keith.conger@gmail.com>
2017-09-10 09:43:56 -0600

    * Messaging when no address or phone number set when task selected

--- NavLINq/TasksTableViewController.swift
+++ NavLINq/TasksTableViewController.swift
@@ -92,51 +92,73 @@ class TasksTableViewController: UITableViewController {
         case 0:
             print("Go Home")
             if let homeAddress = UserDefaults.standard.string(forKey: "gohome_address_preference"){
-                let geocoder = CLGeocoder()
-                geocoder.geocodeAddressString(homeAddress) {
-                    placemarks, error in
-                    let placemark = placemarks?.first
-                    let lat = placemark?.location?.coordinate.latitude
-                    let lon = placemark?.location?.coordinate.longitude
-                    print("Lat: \(lat), Lon: \(lon)")
+                if homeAddress != "" {
+                    let geocoder = CLGeocoder()
+                    geocoder.geocodeAddressString(homeAddress) {
+                        placemarks, error in
+                        let placemark = placemarks?.first
+                        let lat = placemark?.location?.coordinate.latitude
+                        let lon = placemark?.location?.coordinate.longitude
+                        print("Lat: \(lat), Lon: \(lon)")
+                        
+                        let destLatitude: CLLocationDegrees = lat!
+                        let destLongitude: CLLocationDegrees = lon!
+                        let coordinates = CLLocationCoordinate2DMake(destLatitude, destLongitude)
+                        let navPlacemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+                        let mapitem = MKMapItem(placemark: navPlacemark)
+                        let options = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+                        mapitem.openInMaps(launchOptions: options)
+                    }
+                } else {
+                    // the alert view
+                    let alert = UIAlertController(title: "", message: "No Go Home address set in Settings", preferredStyle: .alert)
+                    self.present(alert, animated: true, completion: nil)
                     
-                    let destLatitude: CLLocationDegrees = lat!
-                    let destLongitude: CLLocationDegrees = lon!
-                    let coordinates = CLLocationCoordinate2DMake(destLatitude, destLongitude)
-                    let navPlacemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
-                    let mapitem = MKMapItem(placemark: navPlacemark)
-                    let options = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
-                    mapitem.openInMaps(launchOptions: options)
+                    // change to desired number of seconds (in this case 2 seconds)
+                    let when = DispatchTime.now() + 2
+                    DispatchQueue.main.asyncAfter(deadline: when){
+                        // your code with delay
+                        alert.dismiss(animated: true, completion: nil)
+                    }
                 }
-            } else {
-                // TODO: Do something when no address is set
             }
         case 1:
             print("Call Home")
-            // TODO: TEST THIS
             if let phoneNumber = UserDefaults.standard.string(forKey: "callhome_number_preference"){
-                var validPhoneNumber = ""
-                phoneNumber.characters.forEach {(character) in
-                    switch character {
-                    case "0"..."9":
-                        validPhoneNumber.characters.append(character)
-                    default:
-                        break
-                    }
-                }
-                if let phoneCallURL = URL(string: "telprompt:\(validPhoneNumber)") {
-                    if (UIApplication.shared.canOpenURL(phoneCallURL)) {
-                        if #available(iOS 10, *) {
-                            UIApplication.shared.open(phoneCallURL, options: [:], completionHandler: nil)
-                        } else {
-                            UIApplication.shared.openURL(phoneCallURL as URL)
+                if phoneNumber != "" {
+                    var validPhoneNumber = ""
+                    phoneNumber.characters.forEach {(character) in
+                        switch character {
+                        case "0"..."9":
+                            validPhoneNumber.characters.append(character)
+                        default:
+                            break
                         }
                     }
+                    if let phoneCallURL = URL(string: "telprompt:\(validPhoneNumber)") {
+                        if (UIApplication.shared.canOpenURL(phoneCallURL)) {
+                            if #available(iOS 10, *) {
+                                UIApplication.shared.open(phoneCallURL, options: [:], completionHandler: nil)
+                            } else {
+                                UIApplication.shared.openURL(phoneCallURL as URL)
+                            }
+                        }
+                    }
+                } else {
+                    // the alert view
+                    let alert = UIAlertController(title: "", message: "No Call Home phone number set in Settings", preferredStyle: .alert)
+                    self.present(alert, animated: true, completion: nil)
+                    
+                    // change to desired number of seconds (in this case 2 seconds)
+                    let when = DispatchTime.now() + 2
+                    DispatchQueue.main.asyncAfter(deadline: when){
+                        // your code with delay
+                        alert.dismiss(animated: true, completion: nil)
+                    }
+
                 }
-            } else {
-                // TODO: Do something when no phone number is set
+                
             }
-            
         default:
             print("Unknown Task")
         }
