4bc4ffdcd6630966eb50dde67d5170d450798289
Keith Conger <keith.conger@blackboxembedded.com>
2024-02-10 17:19:26 -0700

    Mix sounds effect, prevents stopping of background music

--- WunderLINQ/Utils/SoundManager.swift
+++ WunderLINQ/Utils/SoundManager.swift
@@ -24,7 +24,7 @@ class SoundManager {
     func playSoundEffect(_ sound: String) {
         if let url = Bundle.main.url(forResource: sound, withExtension: "mp3") {
             do {
-                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [.mixWithOthers])
                 try AVAudioSession.sharedInstance().setActive(true)
 
                 /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
