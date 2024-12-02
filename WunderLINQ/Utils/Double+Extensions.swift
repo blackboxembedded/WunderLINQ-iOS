8631062c4981e117fa495803838340d56829c1e2
Keith Conger <keith.conger@blackboxembedded.com>
2024-12-02 16:35:18 -0700

    If rounded value is -0.0 return 0.0

--- WunderLINQ/Utils/Double+Extensions.swift
+++ WunderLINQ/Utils/Double+Extensions.swift
@@ -22,6 +22,7 @@ extension Double {
     // Rounds the double to decimal places value
     func rounded(toPlaces places:Int) -> Double {
         let divisor = pow(10.0, Double(places))
-        return (self * divisor).rounded() / divisor
+        let result = (self * divisor).rounded() / divisor
+        return result == 0.0 ? 0.0 : result
     }
 }
